29fc0d021a802ceb2e7d52afa41d6148813d3fd6
Greg Kapka <greg@oraclize.it>
2021-10-04 18:24:18 +0100

    feat(contracts): <- adds interface for `ERC20Value` contract

---
+++ contracts/IERC20Vault.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IERC20Vault {
+    function pegIn(
+        uint256 _tokenAmount,
+        address _tokenAddress,
+        string calldata _destinationAddress
+    )
+        external
+        returns (bool);
+
+    function pegIn(
+        uint256 _tokenAmount,
+        address _tokenAddress,
+        string memory _destinationAddress,
+        bytes memory _userData
+    )
+        external
+        returns (bool);
+}
