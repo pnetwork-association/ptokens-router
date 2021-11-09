6498d8983c54bc4349ca00a90cb232e35ba47c77
Greg Kapka <greg@oraclize.it>
2021-11-09 17:37:18 +0000

    feat(CLI): <- adds barebones of that

---
+++ cli.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+const { docopt } = require('docopt')
+const { version } = require('./package.json')
+
+const TOOL_NAME = 'cli.js'
+const HELP_OPTION = '--help'
+const VERSION_OPTION = '--version'
+const VERIFY_CONTRACT_CMD = 'verifyContract'
+
+const USAGE_INFO = `
+❍ pTokens Router Contract Command Line Interface
+
+  Copyright Provable Things 2021
+  Questions: greg@oraclize.it
+
+❍ Info:
+
+  A tool to aid with deployments of & interactions with the upgradeable pToken Router logic contract.
+
+  NOTE: Functions that make transactions require a private key. Please provide a GPG encrpyted file called
+   'private-key.gpg' containing your key in the root of the repository. Create one via:
+   'echo <your-private-key> | gpg -c --output private-key.gpg'
+
+  NOTE: The tool requires a '.env' file to exist in the root of the repository with the following info:
+    ENDPOINT=<rpc-endpoint-for-blochain-to-interact-with>
+
+  NOTE: To call the '${VERIFY_CONTRACT_CMD}' function, the following extra environment variable is required:
+    ETHERSCAN_API_KEY=<api-key-for-automated-contract-verifications>
+
+❍ Usage:
+  ${TOOL_NAME} ${HELP_OPTION}
+  ${TOOL_NAME} ${VERSION_OPTION}
+
+❍ Commands:
+
+❍ Options:
+  ${HELP_OPTION}                ❍ Show this message.
+  ${VERSION_OPTION}             ❍ Show tool version.
+`
+
+const main = _ => {
+  const CLI_ARGS = docopt(USAGE_INFO, { version })
+}
+
+main().catch(_err => console.error('✘', _err.message))
