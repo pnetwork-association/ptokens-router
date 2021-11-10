0e15c1d64fc18b1c8b2c3859facec0937fc22913
Greg Kapka <greg@oraclize.it>
2021-11-10 11:50:30 +0000

    chore(gitignore): <- update that to ignore openzepplin dir

--- contracts/PTokensRouter.sol
+++ contracts/PTokensRouter.sol
@@ -5,17 +5,24 @@ pragma solidity ^0.8.0;
 import "./IERC20Vault.sol";
 import "./ConvertAddressToString.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1820RegistryUpgradeable.sol";
 
-contract PTokensRouter is IERC777Recipient, ConvertAddressToString {
+contract PTokensRouter is
+    Initializable,
+    IERC777Recipient,
+    ConvertAddressToString
+{
     IERC1820RegistryUpgradeable private _erc1820 = IERC1820RegistryUpgradeable(
         0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
     );
     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     mapping(bytes4 => address) public vaultAddresses;
 
-    constructor () {
+    function initialize ()
+        public initializer
+    {
         _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
     }
 
