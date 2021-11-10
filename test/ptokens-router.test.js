abf577f13f365c3c11e7496625e7c2f39f62f69f
Greg Kapka <greg@oraclize.it>
2021-11-10 11:56:53 +0000

    fix(tests): <- use upgradeable deployer in those

--- test/ptokens-router.test.js
+++ test/ptokens-router.test.js
@@ -9,16 +9,16 @@ const {
 const assert = require('assert')
 
 describe('pTokens Router Contract', () => {
-  let DEPLOYED_CONTRACT, CONTRACT_FACTORY
+  let CONTRACT, CONTRACT_FACTORY
 
   beforeEach(async () => {
     const ROUTER_CONTRACT_PATH = 'contracts/PTokensRouter.sol:PTokensRouter'
     CONTRACT_FACTORY = await ethers.getContractFactory(ROUTER_CONTRACT_PATH)
-    DEPLOYED_CONTRACT = await CONTRACT_FACTORY.deploy()
+    CONTRACT = await upgrades.deployProxy(CONTRACT_FACTORY, [])
   })
 
   it('Should decode metadata...', async () => {
-    const result = await DEPLOYED_CONTRACT.decodeMetadata(getSampleMetadata())
+    const result = await CONTRACT.decodeMetadata(getSampleMetadata())
     assert.strictEqual(result.userData, SAMPLE_USER_DATA)
     assert.strictEqual(result.originAddress, SAMPLE_ETH_ADDRESS_1)
     assert.strictEqual(result.metadataVersion, SAMPLE_METADATA_VERSION)
@@ -26,7 +26,7 @@ describe('pTokens Router Contract', () => {
   })
 
   it('Should decode userdata to destination chain and address...', async () => {
-    const result = await DEPLOYED_CONTRACT.decodeUserDataToDestinationChainAndAddress(getSampleUserData())
+    const result = await CONTRACT.decodeUserDataToDestinationChainAndAddress(getSampleUserData())
     assert.strictEqual(result.destinationAddress, SAMPLE_ETH_ADDRESS_1)
     assert.strictEqual(result.destinationChain, SAMPLE_METADATA_CHAIN_ID_1)
   })
