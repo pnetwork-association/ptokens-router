fbdf868bea12519331964e2e161f5d60999549ee
Greg Kapka <greg@oraclize.it>
2021-11-11 11:50:29 +0000

    feat(CLI): <- adds fxn to that to add vault address

--- cli.js
+++ cli.js
@@ -7,16 +7,19 @@ const { getOwner } = require('./lib/get-owner')
 const { transferOwner } = require('./lib/transfer-owner')
 const { deployContract } = require('./lib/deploy-contract')
 const { verifyContract } = require('./lib/verify-contract')
+const { addVaultAddress } = require('./lib/add-vault-address')
 
 const TOOL_NAME = 'cli.js'
 const HELP_OPTION = '--help'
 const NETWORK_ARG = '<network>'
+const CHAIN_ID_ARG = '<chainId>'
 const GET_OWNER_CMD = 'getOwner'
 const VERSION_OPTION = '--version'
 const ETH_ADDRESS_ARG = '<ethAddress>'
 const TRANSFER_OWNER_CMD = 'transferOwner'
 const VERIFY_CONTRACT_CMD = 'verifyContract'
 const DEPLOY_CONTRACT_CMD = 'deployContract'
+const ADD_VAULT_ADDRESS_CMD = 'addVaultAddress'
 const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
 
 const USAGE_INFO = `
@@ -46,18 +49,21 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${GET_OWNER_CMD} ${DEPLOYED_ADDRESS_ARG}
   ${TOOL_NAME} ${TRANSFER_OWNER_CMD} ${DEPLOYED_ADDRESS_ARG} ${ETH_ADDRESS_ARG}
   ${TOOL_NAME} ${VERIFY_CONTRACT_CMD} ${DEPLOYED_ADDRESS_ARG} ${NETWORK_ARG}
+  ${TOOL_NAME} ${ADD_VAULT_ADDRESS_CMD} ${DEPLOYED_ADDRESS_ARG} ${CHAIN_ID_ARG} ${ETH_ADDRESS_ARG}
 
 ❍ Commands:
   ${DEPLOY_CONTRACT_CMD}        ❍ Deploy the logic contract.
   ${VERIFY_CONTRACT_CMD}        ❍ Verify the logic contract.
   ${GET_OWNER_CMD}              ❍ Get the owner of the contract at ${DEPLOYED_ADDRESS_ARG}.
   ${TRANSFER_OWNER_CMD}         ❍ Transfer ownership of contract at ${DEPLOYED_ADDRESS_ARG} to ${ETH_ADDRESS_ARG}.
+  ${ADD_VAULT_ADDRESS_CMD}       ❍ Adds ${ETH_ADDRESS_ARG} as vault address with ${CHAIN_ID_ARG} to ${DEPLOYED_ADDRESS_ARG}.
 
 ❍ Options:
   ${HELP_OPTION}                ❍ Show this message.
   ${VERSION_OPTION}             ❍ Show tool version.
-  ${DEPLOYED_ADDRESS_ARG}     ❍ The ETH address of the deployed pToken.
   ${ETH_ADDRESS_ARG}          ❍ A valid ETH address.
+  ${DEPLOYED_ADDRESS_ARG}     ❍ The ETH address of the deployed pToken.
+  ${CHAIN_ID_ARG}             ❍ A pToken metadata chain ID, as a 'bytes4' solidity type.
   ${NETWORK_ARG}             ❍ Network the contract is deployed on. It must exist in the 'hardhat.config.json'.
 `
 
@@ -71,6 +77,8 @@ const main = _ => {
     return getOwner(CLI_ARGS[DEPLOYED_ADDRESS_ARG])
   if (CLI_ARGS[TRANSFER_OWNER_CMD])
     return transferOwner(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[ETH_ADDRESS_ARG])
+  if (CLI_ARGS[ADD_VAULT_ADDRESS_CMD])
+    return addVaultAddress(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[CHAIN_ID_ARG], CLI_ARGS[ETH_ADDRESS_ARG])
 }
 
 main().catch(_err => console.error('✘', _err.message))
