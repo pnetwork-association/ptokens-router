e213b369c810607e14b038efce6e20025f6c7684
Greg Kapka <greg@oraclize.it>
2021-11-25 17:30:16 +0000

    feat(router): <- adds access control to that & tests

--- contracts/PTokensRouter.sol
+++ contracts/PTokensRouter.sol
@@ -4,27 +4,95 @@ pragma solidity ^0.8.0;
 
 import "./IERC20Vault.sol";
 import "./ConvertAddressToString.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1820RegistryUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 
 contract PTokensRouter is
     Initializable,
-    OwnableUpgradeable,
-    ConvertAddressToString
+    ConvertAddressToString,
+    AccessControlEnumerableUpgradeable
 {
     IERC1820RegistryUpgradeable constant private _erc1820 = IERC1820RegistryUpgradeable(
         0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
     );
     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     mapping(bytes4 => address) public vaultAddresses;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant REDEEMER_ROLE = keccak256("REDEEMER_ROLE");
 
     function initialize ()
         public initializer
     {
-        __Ownable_init();
         _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    modifier onlyAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()) || hasRole(ADMIN_ROLE, _msgSender()),
+            "Caller is not an admin!"
+        );
+        _;
+    }
+
+    modifier onlyMinter() {
+        require(hasRole(ADMIN_ROLE, _msgSender()), "Caller is not a minter!");
+        _;
+    }
+
+    modifier onlyRedeemer() {
+        require(hasRole(ADMIN_ROLE, _msgSender()), "Caller is not a redeemer!");
+        _;
+    }
+
+    function addVaultAddress(
+        bytes4 _chainId,
+        address _vaultAddress
+    )
+        external
+        onlyAdmin
+        returns (bool success)
+    {
+        vaultAddresses[_chainId] = _vaultAddress;
+        return true;
+    }
+
+    function removeVaultAddress(
+        bytes4 _chainId
+    )
+        external
+        onlyAdmin
+        returns (bool success)
+    {
+        delete vaultAddresses[_chainId];
+        return true;
+    }
+
+    function grantMinterRole(address _account) external {
+        grantRole(MINTER_ROLE, _account);
+    }
+
+    function revokeMinterRole(address _account) external {
+        revokeRole(MINTER_ROLE, _account);
+    }
+
+    function hasMinterRole(address _account) external view returns (bool) {
+        return hasRole(MINTER_ROLE, _account);
+    }
+
+    function grantRedeemerRole(address _account) external {
+        grantRole(REDEEMER_ROLE, _account);
+    }
+
+    function revokeRedeemerRole(address _account) external {
+        revokeRole(REDEEMER_ROLE, _account);
+    }
+
+    function hasRedeemerRole(address _account) external view returns (bool) {
+        return hasRole(REDEEMER_ROLE, _account);
     }
 
     function getVaultAddressFromDestinationChainId( // TODO test
@@ -40,26 +108,4 @@ contract PTokensRouter is
         return vaultAddress;
     }
 
-    function addVaultAddress(
-        bytes4 _chainId,
-        address _vaultAddress
-    )
-        external
-        onlyOwner
-        returns (bool success)
-    {
-        vaultAddresses[_chainId] = _vaultAddress;
-        return true;
-    }
-
-    function removeVaultAddress(
-        bytes4 _chainId
-    )
-        external
-        onlyOwner
-        returns (bool success)
-    {
-        delete vaultAddresses[_chainId];
-        return true;
-    }
 }
--- test/3-ptokens-router.test.js
+++ test/3-ptokens-router.test.js
@@ -5,21 +5,35 @@ const {
 const assert = require('assert')
 
 describe('pTokens Router Contract', () => {
-  let ROUTER_CONTRACT, CONTRACT_FACTORY, NON_OWNER, NON_OWNED_ROUTER_CONTRACT
-  const NON_OWNER_ERROR = 'Ownable: caller is not the owner'
+  const getRoleHash = _roleStr =>
+    ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_roleStr))
+
+  let ROUTER_CONTRACT, CONTRACT_FACTORY, NON_ADMIN, NON_ADMIN_ROUTER_CONTRACT, OWNER
+  const ADMIN_ROLE = getRoleHash('ADMIN_ROLE')
+  const MINTER_ROLE = getRoleHash('MINTER_ROLE')
+  const NON_ADMIN_ERROR = 'Caller is not an admin'
+  const REDEEMER_ROLE = getRoleHash('REDEEMER_ROLE')
   const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+  const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000'
 
   beforeEach(async () => {
     const ROUTER_CONTRACT_PATH = 'contracts/PTokensRouter.sol:PTokensRouter'
     const signers = await ethers.getSigners()
-    NON_OWNER = signers[1]
+    OWNER = signers[0]
+    NON_ADMIN = signers[1]
     CONTRACT_FACTORY = await ethers.getContractFactory(ROUTER_CONTRACT_PATH)
     ROUTER_CONTRACT = await upgrades.deployProxy(CONTRACT_FACTORY, [])
-    NON_OWNED_ROUTER_CONTRACT = ROUTER_CONTRACT.connect(NON_OWNER)
+    NON_ADMIN_ROUTER_CONTRACT = ROUTER_CONTRACT.connect(NON_ADMIN)
   })
 
-  describe('Ownership tests...', () => {
-    it('Owner can add vault addresses', async () => {
+  describe('Initialization Tests', () => {
+    it('Deploying account should have admin role', async () => {
+      assert(await ROUTER_CONTRACT.hasRole(DEFAULT_ADMIN_ROLE, OWNER.address))
+    })
+  })
+
+  describe('Admin Tests', () => {
+    it('Admin can add vault addresses', async () => {
       const vaultAddressBefore = await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1)
       assert.strictEqual(vaultAddressBefore, ZERO_ADDRESS)
       await ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
@@ -27,7 +41,7 @@ describe('pTokens Router Contract', () => {
       assert.strictEqual(vaultAddressAfter, SAMPLE_ETH_ADDRESS_1)
     })
 
-    it('Owner can remove vault addresses', async () => {
+    it('Admin can remove vault addresses', async () => {
       await ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
       const vaultAddressBefore = await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1)
       assert.strictEqual(vaultAddressBefore, SAMPLE_ETH_ADDRESS_1)
@@ -38,10 +52,10 @@ describe('pTokens Router Contract', () => {
 
     it('Non owner cannot add vault addresses', async () => {
       try {
-        await NON_OWNED_ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
+        await NON_ADMIN_ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
         assert.fail('Should not have succeeded!')
       } catch (_err) {
-        assert(_err.message.includes(NON_OWNER_ERROR))
+        assert(_err.message.includes(NON_ADMIN_ERROR))
       }
     })
 
@@ -49,47 +63,121 @@ describe('pTokens Router Contract', () => {
       ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), SAMPLE_ETH_ADDRESS_1)
       try {
-        await NON_OWNED_ROUTER_CONTRACT.removeVaultAddress(SAMPLE_METADATA_CHAIN_ID_1)
+        await NON_ADMIN_ROUTER_CONTRACT.removeVaultAddress(SAMPLE_METADATA_CHAIN_ID_1)
         assert.fail('Should not have succeeded!')
       } catch (_err) {
-        assert(_err.message.includes(NON_OWNER_ERROR))
+        assert(_err.message.includes(NON_ADMIN_ERROR))
       }
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), SAMPLE_ETH_ADDRESS_1)
     })
   })
 
   describe('Adding & Removing Vault Addresses', () => {
-    it('Owner can add vault address', async () => {
+    it('Admin can add vault address', async () => {
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), ZERO_ADDRESS)
       await ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), SAMPLE_ETH_ADDRESS_1)
     })
 
-    it('Non-owner cannot add vault address', async () => {
+    it('Non admin cannot add vault address', async () => {
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), ZERO_ADDRESS)
       try {
-        await NON_OWNED_ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
+        await NON_ADMIN_ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
         assert.fail('Should not have succeeded!')
       } catch (_err) {
-        assert(_err.message.includes(NON_OWNER_ERROR))
+        assert(_err.message.includes(NON_ADMIN_ERROR))
       }
     })
 
-    it('Owner can remove vault address', async () => {
+    it('Admin can remove vault address', async () => {
       await ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), SAMPLE_ETH_ADDRESS_1)
       await ROUTER_CONTRACT.removeVaultAddress(SAMPLE_METADATA_CHAIN_ID_1)
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), ZERO_ADDRESS)
     })
 
-    it('Non-owner cannot remove vault address', async () => {
+    it('Non admin cannot remove vault address', async () => {
       await ROUTER_CONTRACT.addVaultAddress(SAMPLE_METADATA_CHAIN_ID_1, SAMPLE_ETH_ADDRESS_1)
       assert.strictEqual(await ROUTER_CONTRACT.vaultAddresses(SAMPLE_METADATA_CHAIN_ID_1), SAMPLE_ETH_ADDRESS_1)
       try {
-        await NON_OWNED_ROUTER_CONTRACT.removeVaultAddress(SAMPLE_METADATA_CHAIN_ID_1)
+        await NON_ADMIN_ROUTER_CONTRACT.removeVaultAddress(SAMPLE_METADATA_CHAIN_ID_1)
         assert.fail('Should not have succeeded!')
       } catch (_err) {
-        assert(_err.message.includes(NON_OWNER_ERROR))
+        assert(_err.message.includes(NON_ADMIN_ERROR))
+      }
+    })
+  })
+
+  describe('Minter Role Tests', () => {
+    it('Admin can grant minter role', async () => {
+      assert(!await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+      ROUTER_CONTRACT.grantMinterRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+    })
+
+    it('Non admin cannot grant minter role', async () => {
+      try {
+        await NON_ADMIN_ROUTER_CONTRACT.grantMinterRole(SAMPLE_ETH_ADDRESS_1)
+        assert.fail('Should not have succeeded!')
+      } catch (_err) {
+        assert(_err.message.includes('missing role'))
+        assert(!await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+      }
+    })
+
+    it('Admin can revoke minter role', async () => {
+      ROUTER_CONTRACT.grantMinterRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+      ROUTER_CONTRACT.revokeMinterRole(SAMPLE_ETH_ADDRESS_1)
+      assert(!await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+    })
+
+    it('Non admin cannot revoke minter role', async () => {
+      ROUTER_CONTRACT.grantMinterRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+      try {
+        await NON_ADMIN_ROUTER_CONTRACT.grantMinterRole(SAMPLE_ETH_ADDRESS_1)
+        assert.fail('Should not have succeeded!')
+      } catch (_err) {
+        assert(_err.message.includes('missing role'))
+        assert(await ROUTER_CONTRACT.hasMinterRole(SAMPLE_ETH_ADDRESS_1))
+      }
+    })
+  })
+
+  describe('Redeemer Role Tests', () => {
+    it('Admin can grant redeemer role', async () => {
+      assert(!await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+      ROUTER_CONTRACT.grantRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+    })
+
+    it('Non admin cannot grant redeemer role', async () => {
+      try {
+        await NON_ADMIN_ROUTER_CONTRACT.grantRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+        assert.fail('Should not have succeeded!')
+      } catch (_err) {
+        assert(_err.message.includes('missing role'))
+        assert(!await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+      }
+    })
+
+    it('Admin can revoke redeemer role', async () => {
+      ROUTER_CONTRACT.grantRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+      ROUTER_CONTRACT.revokeRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+      assert(!await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+    })
+
+    it('Non admin cannot revoke redeemer role', async () => {
+      ROUTER_CONTRACT.grantRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+      assert(await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
+      try {
+        await NON_ADMIN_ROUTER_CONTRACT.grantRedeemerRole(SAMPLE_ETH_ADDRESS_1)
+        assert.fail('Should not have succeeded!')
+      } catch (_err) {
+        assert(_err.message.includes('missing role'))
+        assert(await ROUTER_CONTRACT.hasRedeemerRole(SAMPLE_ETH_ADDRESS_1))
       }
     })
   })
