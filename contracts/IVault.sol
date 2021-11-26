722af2904a6d19732bb74d19c72c7ff94f4caced
Greg Kapka <greg@oraclize.it>
2021-11-26 10:19:03 +0000

    ref(contracts): <- rename the vault interface in those for brevity

--- contracts/IVault.sol
+++ contracts/IVault.sol
@@ -2,13 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-interface IERC20Vault {
+interface IVault {
     function pegIn(
         uint256 _tokenAmount,
         address _tokenAddress,
         string memory _destinationAddress,
+        bytes4 _destinationChainId,
         bytes memory _userData
-    )
-        external
-        returns (bool);
+    ) external returns (bool);
 }
