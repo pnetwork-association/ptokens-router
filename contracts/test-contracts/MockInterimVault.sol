a51a448d471d18626ead41a6e34bae1f91b17453
Greg Kapka <greg@oraclize.it>
2021-11-30 15:03:35 +0000

    feat(mock-vault): <- registers that as ERC777 recipient

--- contracts/test-contracts/MockInterimVault.sol
+++ contracts/test-contracts/MockInterimVault.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
+
 pragma solidity ^0.8.0;
 
 contract MockInterimVault {
     bytes4 public  ORIGIN_CHAIN_ID;
+    IERC1820Registry constant private ERC1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 
     constructor(bytes4 originChainId) {
         ORIGIN_CHAIN_ID = originChainId;
+        ERC1820.setInterfaceImplementer(address(this), keccak256("ERC777TokensRecipient"), address(this));
     }
 
     event PegInCalled(
