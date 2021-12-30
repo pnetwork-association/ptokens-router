300587616cd228d55962908cdd156efb060cbb9c
Greg Kapka <greg@oraclize.it>
2021-12-30 15:09:17 +0000

    feat(CLI): <- adds fxn to that to show wallet details

--- cli.js
+++ cli.js
@@ -9,6 +9,7 @@ const { transferOwner } = require('./lib/transfer-owner')
 const { deployContract } = require('./lib/deploy-contract')
 const { verifyContract } = require('./lib/verify-contract')
 const { addVaultAddress } = require('./lib/add-vault-address')
+const { showWalletDetails } = require('./lib/show-wallet-details')
 const { removeVaultAddress } = require('./lib/remove-vault-address')
 const { getEncodedInitArgs } = require('./lib/get-encoded-init-args')
 const { showExistingContractAddresses } = require('./lib/show-existing-contracts')
@@ -26,6 +27,7 @@ const DEPLOY_CONTRACT_CMD = 'deployContract'
 const ADD_VAULT_ADDRESS_CMD = 'addVaultAddress'
 const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
 const GET_ENCODED_INIT_ARGS_CMD = 'encodeInitArgs'
+const SHOW_WALLET_DETAILS_CMD = 'showWalletDetails'
 const REMOVE_VAULT_ADDRESS_CMD = 'removeVaultAddress'
 const SHOW_EXISTING_CONTRACTS_CMD = 'showExistingContracts'
 
@@ -53,6 +55,7 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${HELP_OPTION}
   ${TOOL_NAME} ${VERSION_OPTION}
   ${TOOL_NAME} ${DEPLOY_CONTRACT_CMD}
+  ${TOOL_NAME} ${SHOW_WALLET_DETAILS_CMD}
   ${TOOL_NAME} ${SHOW_EXISTING_CONTRACTS_CMD}
   ${TOOL_NAME} ${GET_OWNER_CMD} ${DEPLOYED_ADDRESS_ARG}
   ${TOOL_NAME} ${GET_ENCODED_INIT_ARGS_CMD} ${ETH_ADDRESS_ARG}
@@ -66,10 +69,12 @@ const USAGE_INFO = `
   ${VERIFY_CONTRACT_CMD}        ❍ Verify the logic contract.
   ${GET_OWNER_CMD}              ❍ Get the owner of the contract at ${DEPLOYED_ADDRESS_ARG}.
   ${REMOVE_VAULT_ADDRESS_CMD}    ❍ Removess vault address with ${CHAIN_ID_ARG} from ${DEPLOYED_ADDRESS_ARG}.
+  ${SHOW_WALLET_DETAILS_CMD}     ❍ Decrypts the private key and shows address & balance information.
   ${GET_ENCODED_INIT_ARGS_CMD}        ❍ Calculate the initializer function arguments in ABI encoded format.
   ${TRANSFER_OWNER_CMD}         ❍ Transfer ownership of contract at ${DEPLOYED_ADDRESS_ARG} to ${ETH_ADDRESS_ARG}.
   ${ADD_VAULT_ADDRESS_CMD}       ❍ Adds ${ETH_ADDRESS_ARG} as vault address with ${CHAIN_ID_ARG} to ${DEPLOYED_ADDRESS_ARG}.
   ${SHOW_EXISTING_CONTRACTS_CMD} ❍ Show list of existing pToken logic contract addresses on various blockchains.
+
 
 ❍ Options:
   ${HELP_OPTION}                ❍ Show this message.
@@ -96,6 +101,8 @@ const main = _ => {
     return removeVaultAddress(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[CHAIN_ID_ARG])
   if (CLI_ARGS[GET_ENCODED_INIT_ARGS_CMD])
     return getEncodedInitArgs(CLI_ARGS[ETH_ADDRESS_ARG])
+  if (CLI_ARGS[SHOW_WALLET_DETAILS_CMD])
+    return showWalletDetails()
   if (CLI_ARGS[SHOW_EXISTING_CONTRACTS_CMD])
     return showExistingContractAddresses()
 }
